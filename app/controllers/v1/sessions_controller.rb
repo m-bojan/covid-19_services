3ee97e4fecac93fa3686314732dca2ee2d703cdf
MaJeD BoJaN <bojanmajed@gmail.com>
2020-03-27 14:02:04 +0300

    fix syntax error

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -9,12 +9,12 @@ class V1::SessionsController < V1::BaseController
 
     @admin = Admin.login(login_params.to_h.symbolize_keys)
 
-    return render_bad_request(error: 1308) unless @admin.present?
-    return render_bad_request(error: @admin) if @admin.errors.any?
-    return render_bad_request(error: 1305) if @admin.inactive?
+    return render_bad_request(error: 1308) if @admin == false
+    # return render_bad_request(error: @admin) if @admin.errors.any?
+    # return render_bad_request(error: 1305) if @admin.inactive?
 
     data = { data: { admin: @admin.as_api_response(show_template), token: generate_token(@admin) } }
-    render_success(data: data, message: I18n.t('sessions.login_successfully'))
+    render_success(data: data, message: 'تم تسجيل الدخول بنجاح')
   end
 
   private
